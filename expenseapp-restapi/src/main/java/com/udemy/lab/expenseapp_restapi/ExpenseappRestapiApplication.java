b2f6407946823626b19de21a272f55da8286a7f3
Kannan Thangappan <kannanthangappan@f0-18-98-84-df-c3.local.tld>
2025-05-14 07:45:00 -0500

    add rest and web

---
+++ expenseapp-restapi/src/main/java/com/udemy/lab/expenseapp_restapi/ExpenseappRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.lab.expenseapp_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseappRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseappRestapiApplication.class, args);
+	}
+
+}
---
+++ expenseapp-restapi/src/test/java/com/udemy/lab/expenseapp_restapi/ExpenseappRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.lab.expenseapp_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseappRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
